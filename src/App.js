7664c6a1d2862d505a01a1bedbd701320e5ecfeb
Andres Barroso <abarroso@nectia.com>
2024-03-18 01:27:09 -0300

    Estructura inicial pagina profesional

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./assets/css/main.css"
+import "./assets/css/socials.css";
+import discord from "./assets/img/discord.svg";
+import github from "./assets/img/github.svg";
+import linkedin from "./assets/img/linkedin.svg";
+import whatsapp from "./assets/img/whatsapp.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <h1 className="name">Andrés Barroso</h1>
+        <h3 className="professional-title ">Software Engineer</h3>
+        <div class="social-container">
+          <a class="social linkedin svg" href="https://www.linkedin.com/in/andresbarrosodev/" target="_blank" rel="noreferrer">
+            <object
+              data={linkedin}
+              width="50"
+              height="33"
+              aria-label="Link to Andrés Barroso linkedin"
+            ></object>
+          </a>
+          <a class="social github svg" href="https://github.com/AndresBarrosoDev" target="_blank" rel="noreferrer">
+            <object
+              data={github}
+              width="50"
+              height="33"
+              aria-label="Link to Andrés Barroso github"
+            ></object>
+          </a>
+          <a
+            href="https://wa.me/56957107029"
+            class="social whatsapp svg"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <object
+              data={whatsapp}
+              width="50"
+              height="33"
+              aria-label="Link to Andrés Barroso whatsapp"
+            ></object>
+          </a>
+          <a class="social discord svg" href target="_blank" rel="noreferrer">
+            <object
+              data={discord}
+              width="50"
+              height="33"
+              aria-label="Link to Andrés Barroso discord"
+            ></object>
+          </a>
+        </div>
       </header>
     </div>
   );
