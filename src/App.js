01ecd59302acdd945084768699c4fcc00e328fed
Andres Barroso <abarroso@nectia.com>
2024-03-19 17:25:59 -0300

    New deployment. Added delay on links for users to see hover effect on social buttons. Fxed placement issue for phones.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import React from "react";
 import "./App.css";
-import "./assets/css/main.css"
+import "./assets/css/main.css";
 import "./assets/css/socials.css";
 import discord from "./assets/img/discord.svg";
 import github from "./assets/img/github.svg";
@@ -7,51 +8,80 @@ import linkedin from "./assets/img/linkedin.svg";
 import whatsapp from "./assets/img/whatsapp.svg";
 
 function App() {
+  const openLink = (event, url) => {
+    event.preventDefault();
+    setTimeout(() => {
+      window.open(url, "_blank");
+    }, 500); 
+  };
+
   return (
     <div className="app">
-      <header className="app-header">
+      <main className="app-container">
         <h1 className="name">Andrés Barroso</h1>
         <h3 className="professional-title ">Software Engineer</h3>
-        <div class="social-container">
-          <a class="social linkedin svg" href="https://www.linkedin.com/in/andresbarrosodev/" target="_blank" rel="noreferrer">
+        <div className="social-container">
+          <a
+            className="social linkedin svg"
+            href="https://www.linkedin.com/in/andresbarrosodev/"
+            onClick={(event) =>
+              openLink(
+                event,
+                "https://www.linkedin.com/in/andresbarrosodev/"
+              )
+            }
+          >
             <object
               data={linkedin}
               width="50"
               height="33"
-              aria-label="Link to Andrés Barroso linkedin"
+              aria-label="Link to Andrés Barroso's linkedin"
             ></object>
           </a>
-          <a class="social github svg" href="https://github.com/AndresBarrosoDev" target="_blank" rel="noreferrer">
+          <a
+            className="social github svg"
+            href="https://github.com/AndresBarrosoDev"
+            onClick={(event) =>
+              openLink(event, "https://github.com/AndresBarrosoDev")
+            }
+          >
             <object
               data={github}
               width="50"
               height="33"
-              aria-label="Link to Andrés Barroso github"
+              aria-label="Link to Andrés Barroso's github"
             ></object>
           </a>
           <a
             href="https://wa.me/56957107029"
-            class="social whatsapp svg"
-            target="_blank"
-            rel="noreferrer"
+            className="social whatsapp svg"
+            onClick={(event) =>
+              openLink(event, "https://wa.me/56957107029")
+            }
           >
             <object
               data={whatsapp}
               width="50"
               height="33"
-              aria-label="Link to Andrés Barroso whatsapp"
+              aria-label="Link to Andrés Barroso's whatsapp"
             ></object>
           </a>
-          <a class="social discord svg" href target="_blank" rel="noreferrer">
+          <a
+            className="social discord svg"
+            href
+            onClick={(event) =>
+              openLink(event, "https://your-discord-link")
+            }
+          >
             <object
               data={discord}
               width="50"
               height="33"
-              aria-label="Link to Andrés Barroso discord"
+              aria-label="Link to Andrés Barroso's discord"
             ></object>
           </a>
         </div>
-      </header>
+      </main>
     </div>
   );
 }
